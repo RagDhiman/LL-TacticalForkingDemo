79ad92f0682e0b511233b6883db3d7941bc16151
Rag <Rag@RAGSMINIPC>
2022-09-21 12:53:30 +0100

    test

--- 00-Before-TF/ShopData/ShopContext.cs
+++ 00-Before-TF/ShopData/ShopContext.cs
@@ -3,7 +3,7 @@ using ShopDomain;
 
 namespace ShopData
 {
-    public class ShopContext: DbContext
+    public class ShopContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Account> Address { get; set; }
@@ -23,6 +23,38 @@ namespace ShopData
             optionsBuilder.UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ShopDatabase"
             );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var myAccounts = new List<Account>() {
+                new Account() { AccountId=1, Name = "Mr Smith", EmailAddress = "Smith@tacticalforking.com", CreatedDate = new DateTime(2023,12,01) },
+                new Account() { AccountId=2, Name = "Mr Jones", EmailAddress = "Jones@tacticalforking.com", CreatedDate = new DateTime(2023,12,02) }
+            };
+
+            modelBuilder.Entity<Account>().HasData(myAccounts);
+
+            var myAddresses = new List<Address>() {
+                new Address() { AddressId=1, AccountId=1,  AddressLine1 = "403 Florence Road", AddressLine2 = "Smethwick",  AddressLine3="West Midlands", CityTown = "Birmingham", PostCode = "B12345"},
+                new Address() { AddressId=2, AccountId=2,  AddressLine1 = "501 Parkhill Road", AddressLine2 = "Blue Gates",  AddressLine3="West Midlands", CityTown = "Birmingham", PostCode = "B67890"},
+            };
+
+            modelBuilder.Entity<Address>().HasData(myAddresses);
+
+            var myCreditCard = new List<CreditCard>() {
+                new CreditCard() { CreditCardId=1, AccountId=1, CreditCardName = "Mr A Smith", CreditCardNumber = 123456789, CreditDate =new DateTime(2023,12,01)},
+                new CreditCard() { CreditCardId=2, AccountId=2, CreditCardName = "Mr B Jones", CreditCardNumber = 987654321, CreditDate=new DateTime(2023,12,01)}
+            };
+
+            modelBuilder.Entity<CreditCard>().HasData(myCreditCard);
+
+            var myBillingAddress = new List<BillingAddress>() {
+                new BillingAddress() { BillingAddressId=1, CreditCardId=1,  AddressLine1 = "403 Florence Road", AddressLine2 = "Smethwick",  AddressLine3="West Midlands", CityTown = "Birmingham", PostCode = "B12345"},
+                new BillingAddress() { BillingAddressId=2, CreditCardId=2,  AddressLine1 = "501 Parkhill Road", AddressLine2 = "Blue Gates",  AddressLine3="West Midlands", CityTown = "Birmingham", PostCode = "B67890"},
+            };
+
+            modelBuilder.Entity<BillingAddress>().HasData(myBillingAddress);
+
         }
     }
 }
--- 00-Before-TF/ShopDomain/CreditCard.cs
+++ 00-Before-TF/ShopDomain/CreditCard.cs
@@ -3,8 +3,10 @@ namespace ShopDomain
     public class CreditCard
     {
         public int CreditCardId { get; set; }
+        public int AccountId { get; set; }
         public string CreditCardName { get; set; }
         public int CreditCardNumber { get; set; }
         public DateTime CreditDate { get; set; }
+        public Account Account { get; set; }
     }
 }
